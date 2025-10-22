cecd58af03fb2f17cff900dd253ec2b25cfe3744
CanFam23 <nickclouse03@gmail.com>
2025-10-22 13:48:09 -0600

    Update run simulation to update ini file with input file name

--- src/sim/simulation.py
+++ src/sim/simulation.py
@@ -119,18 +119,39 @@ def smet_to_csv(data_source: str, output_file_path: str,output_file_name: str):
     
     df.to_csv(os.path.join(output_file_path,output_file_name), index=False)
     
-def run_simulation(file_path):
+def run_simulation(file_path, ini_file_path):
     output_files = []
     for file in os.listdir(file_path):
         print(f"Running simulation on {file}")
+        
         df = pd.read_csv(os.path.join(file_path, file))
+        fxx = int(df['fxx'].unique()[0])
+        id = int(df['point_id'].unique()[0])
         df['time'] = pd.to_datetime(df['time'])
-        csv_to_smet(df, file, "data/input", f"{file.split('.')[0]}.smet")
+        smet_name = f"{file.split('.')[0]}.smet"
+        csv_to_smet(df, file, "data/input", smet_name)
+        
+        # Put input file in ini file so SNOWPACK uses correct file
+        with open(ini_file_path, 'r') as ini_file:
+            lines = ini_file.readlines()
+        for i in range(len(lines)):
+            if 'STATION1' in lines[i]:
+                lines[i] = f'STATION1 = {smet_name}\n'
+                break
+        with open(ini_file_path, "w") as ini_file:
+            ini_file.writelines(lines)
+        
+        # Run snowpack
         result = subprocess.run(["/Applications/Snowpack/bin/snowpack", "-b", df['time'].min().isoformat(), "-e", df['time'].max().isoformat(), "-c", "/Users/nickclouse/Desktop/senior-proj/avy-forecasting/data/input/avyIO.ini"], capture_output=True, text=True)
 
-        if result.returncode != -11:
+        # Codes -11 and 0 usually mean SNOWPACK ran successfully
+        print(result.returncode)
+        if result.returncode not in [-11,0]:
+            print(result.stderr)
             print(result.stdout)
-        smet_to_csv("data/output/159_Avy_forc.smet","data/sim_output",f"{file.split('.')[0]}_output.csv")
+        
+        # Convert smet data to csv
+        smet_to_csv(f"data/output/{id}_Avy_forc.smet","data/sim_output",f"{file.split('.')[0]}_output.csv")
         output_files.append(os.path.join("data/sim_output",f"{file.split('.')[0]}_output.csv"))
             
     merged_df = pd.DataFrame()
@@ -144,9 +165,9 @@ def run_simulation(file_path):
     
     os.makedirs("data/training_data", exist_ok=True)
 
-    merged_df.to_csv(f"data/training_data/snow_{merged_df['timestamp'].min().year}-{merged_df['timestamp'].max().year}_p{merged_df['id'].unique()[0]}.csv",index=False)        
+    merged_df.to_csv(f"data/training_data/snow_{merged_df['timestamp'].min().year}-{merged_df['timestamp'].max().year}_p{id}_fxx{fxx}.csv",index=False)         # type: ignore
 
             
 if __name__ == "__main__":
-    fp = "../data/FAC/2020-10-01_00_2025-06-01_00_159_160_0_1/weather_2020-2025_p159_fxx1"
-    run_simulation(fp)
+    fp = "../data/FAC/2020-10-01_00_2025-06-01_00_159_160_0_1/weather_2020-2025_p160_fxx1"
+    run_simulation(fp,"data/input/avyIO.ini")
