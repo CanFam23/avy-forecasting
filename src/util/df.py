8a7b3e06b2f49c5e72691dfc1e4ce3e54729f448
CanFam23 <nickclouse03@gmail.com>
2025-11-03 09:35:33 -0700

    Fix remove outliers to remove less, use prev and next hours data:

--- src/util/df.py
+++ src/util/df.py
@@ -1,28 +1,34 @@
 import pandas as pd
 
+from datetime import timedelta
+
 from src import REQ_COLS, EXP_COLS
 
 def remove_outliers(df: pd.DataFrame) -> pd.DataFrame:
     for c in df.columns:
-        if c in ['time','point_id','fxx','valid_time','sde']:
+        if c in ['time','point_id','fxx','valid_time']:
             continue
             
-        min_val = -1000
+        min_val = -10
         max_val = 1000
         
         bad_vals = df[(df[c] > max_val) | (df[c] < min_val)]
 
         if bad_vals.shape[0] != 0:
             for index, row in bad_vals.iterrows():
+                # Get data from prev and next hours
+                prev = df[(df['time'] == row['time'] - timedelta(hours=1)) & (df['point_id'] == row['point_id'])]
+                next = df[(df['time'] == row['time'] + timedelta(hours=1)) & (df['point_id'] == row['point_id'])]
+
                 if index == 0:
                     # First row, only next row exists
-                    df.loc[index, c] = df.loc[index + 1, c]
+                    df.loc[index, c] = next[c].values[0]
                 elif index == df.shape[0] - 1:
                     # Last row, only previous row exists
-                    df.loc[index, c] = df.loc[index - 1, c]
+                    df.loc[index, c] = prev[c].values[0]
                 else:
                     # Middle rows, average of previous and next
-                    df.loc[index, c] = (df.loc[index - 1, c] + df.loc[index + 1, c]) / 2 # type: ignore
+                    df.loc[index, c] = (next[c].values[0] + prev[c].values[0]) / 2 # type: ignore
             print(f"Replaced {bad_vals.shape[0]} major outliers in col {c}")
     return df
 
